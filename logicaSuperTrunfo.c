7ec6681702af4add6ccd57f3fa4bfff81a2c6e11
Nikolas-Pereira <163581471+Nikolas-Pereira@users.noreply.github.com>
2025-02-03 20:22:35 +0000

    finalizado codigo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 
+int main() {
+    char Campo_limpo[100], Jundiai[200];
+    int populacao_Campo_limpo, populacao_Jundiai;
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", &Campo_limpo);
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", &Jundiai);
+
+    printf("Digite a população da cidade:\n");
+    scanf("%d", &populacao_Campo_limpo);
+
+    printf("Digite a população da cidade:\n");
+    scanf("%d", &populacao_Jundiai);
+
+   if (populacao_Campo_limpo > populacao_Jundiai) {
+        printf("Campo limpo tem maior população.\n");
+    } else if (populacao_Jundiai > populacao_Campo_limpo) {
+        printf("Jundiai tem maior população!\n");
+    } else {
+
+        printf("Jundiai tem mais população! \n");
+    }
+    }
+
+
+
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
@@ -25,6 +52,7 @@ int main() {
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
 
+ 
     // Exemplo:
     // if (populacaoA > populacaoB) {
     //     printf("Cidade 1 tem maior população.\n");
@@ -39,5 +67,3 @@ int main() {
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
-    return 0;
-}
